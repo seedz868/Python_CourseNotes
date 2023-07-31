9cc410cdab62a24eaff095ff45cd72049ff24152
Ahmad Saeed Rajab <46015666+seedz868@users.noreply.github.com>
2023-07-30 23:24:40 -0400

    Create coding_practice.py

---
+++ coding_practice.py
@@ -0,0 +1,5 @@
+# dictionary init
+dS[letter] = 1 + dS.get(letter,0)
+
+https://docs.google.com/spreadsheets/d/1A2PaQKcdwO_lwxz9bAnxXnIQayCouZP6d-ENrBz_NXc/edit#gid=0
+https://github.com/neetcode-gh/leetcode/tree/main/python
