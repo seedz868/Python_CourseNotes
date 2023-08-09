444224432a64caecdd50219d32e3cbcdc1ca5912
Ahmad Saeed Rajab <46015666+seedz868@users.noreply.github.com>
2023-08-08 23:10:13 -0400

    Update coding_practice.py

--- coding_practice.py
+++ coding_practice.py
@@ -3,3 +3,42 @@ dS[letter] = 1 + dS.get(letter,0)
 
 https://docs.google.com/spreadsheets/d/1A2PaQKcdwO_lwxz9bAnxXnIQayCouZP6d-ENrBz_NXc/edit#gid=0
 https://github.com/neetcode-gh/leetcode/tree/main/python
+
+
+# String funcs
+astring = "Hello world!"
+print(astring.index("o"))
+
+x = astring + "xxx"
+
+astring.startswith("Hello")
+astring.endswith("ld!")
+afewwords = astring.split(" ")
+
+print(astring.upper())
+print(astring.lower())
+print (p.isnumeric()) 
+print ('DD'.isupper())
+print(astring[::-1])
+print (astring[3:7])
+
+print (ord('Z'))
+print (ord('9'))
+print (chr(66))
+
+# Array funcs
+q.sort(reverse=True)
+
+qq = ['b','a', '6']
+print (''.join(qq))
+#ba6
+
+arr = ['4','3','2','1']
+print (arr.pop()) #1
+print (arr.pop(0)) # 4
+
+arr.insert(0, '9') #insert at 0
+
+
+
+
